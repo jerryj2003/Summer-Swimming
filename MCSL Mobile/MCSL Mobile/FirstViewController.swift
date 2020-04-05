af40e775b088973cea5a22a7d3d685f1800a9229
Jerry Ji <jerryji2003@gmail.com>
2020-04-05 14:21:06 -0400

    Set up project

---
+++ MCSL Mobile/MCSL Mobile/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MCSL Mobile
+//
+//  Created by Jerry Ji on 4/5/20.
+//  Copyright © 2020 Jerry Ji. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
