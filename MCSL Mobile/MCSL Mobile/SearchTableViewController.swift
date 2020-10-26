8f119b17272af0d550e9e6c4589c2b0333e97829
Jerry Ji <jerryji2003@gmail.com>
2020-10-26 12:05:53 -0400

    Added loading indicator for search function Added indictor when no results are found for search function Set universal color for tab bar and navigation bar More themes and icons to be implemented

--- MCSL Mobile/MCSL Mobile/SearchTableViewController.swift
+++ MCSL Mobile/MCSL Mobile/SearchTableViewController.swift
@@ -8,29 +8,36 @@
 
 import UIKit
 import CodableFirebase
+import DZNEmptyDataSet
 
-class SearchTableViewController: UITableViewController {
+class SearchTableViewController: UITableViewController, DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
     
     var searchTerm : String?{
         didSet{
             timer?.invalidate()
-            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { (_) in
+            timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { (_) in
                 self.update()
+            }
+            if searchTerm?.isEmpty == false{
+                loadingIndicator.startAnimating()
+                tableView.reloadData()
             }
         }
     }
     var searchResult = [(name: String, ID: String)]()
     var timer : Timer?
+    var loadingIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
     
     func update() {
         guard searchTerm != nil && searchTerm != ""
         else {
+            update(to: [], for: searchTerm)
             return
         }
+        let searchTermCopy = searchTerm!
         ref.child("persons").queryOrdered(byChild: "searchTerm").queryStarting(atValue: searchTerm?.lowercased()).queryEnding(atValue: searchTerm!.lowercased() + "\u{f8ff}").observeSingleEvent(of: .value) { (snapshot) in
             guard let value = snapshot.value else {
-                self.searchResult = []
-                self.tableView.reloadData()
+                self.update(to: [], for: searchTermCopy)
                 return
             }
             DispatchQueue.global().async {
@@ -38,28 +45,53 @@ class SearchTableViewController: UITableViewController {
                     var name : String
                 }
                 do {
+                    if self.searchTerm != searchTermCopy{
+                        return
+                    }
                     let resultDict = try FirebaseDecoder().decode([String:SimpleSwimmer].self, from: value).sorted(by: { (swimmer1, swimmer2) -> Bool in
                         swimmer1.value.name < swimmer2.value.name
                     })
-                    self.searchResult = resultDict.map({ (arg0) in
+                    let searchResultCopy : [(name: String, ID: String)]
+                    searchResultCopy = resultDict.map({ (arg0) in
                         let (ID, value) = arg0
                         return (value.name, ID)
                     })
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
+                    self.update(to: searchResultCopy, for: searchTermCopy)
                 } catch {
                     print(error)
+                    self.update(to: [], for: searchTermCopy)
                 }
-                
             }
         }
     }
     
+    func update(to results: [(name: String, ID: String)], for searchTerm: String?){
+        if searchTerm == self.searchTerm{
+            searchResult = results
+            DispatchQueue.main.async {
+                self.loadingIndicator.stopAnimating()
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func title(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
+        
+        var emptyText = "No Swimmers Found"
+        if loadingIndicator.isAnimating{
+            emptyText = "Searching..."
+        }
+        return NSAttributedString(string: emptyText)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        tableView.emptyDataSetSource = self
+        tableView.emptyDataSetDelegate = self
+        loadingIndicator.center = view.center
+        loadingIndicator.hidesWhenStopped = true
+        view.addSubview(loadingIndicator)
+        loadingIndicator.color = #colorLiteral(red: 0.9637133479, green: 0.7429386973, blue: 0.3061919808, alpha: 1)
     }
     
     // MARK: - Table view data source
